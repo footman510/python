72bfa4097dbf969998ad672b6585095bd9f92cc6
footman510 <3112613918@qq.com>
2017-06-28 19:19:54 -0700

    objecct

---
+++ printf.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+printf "%-5s %-10s %-4s\n" No Name Mark
+printf "%-5s %-10s %-4.2f\n" 1 Jack 80.454
+printf "%-5s %-10s %-4.2f\n" 2 Mike 90.5
+printf "%-5s %-10s %-4.2f\n" 3 Lily 88.45
